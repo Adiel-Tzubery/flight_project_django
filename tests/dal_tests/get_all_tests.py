0046ca6c712c33c59de8568e241ead4d28f7143c
adiel tzubery <adieltzubery94@gmail.com>
2023-05-29 03:49:00 +0300

    commit first update changes on test

--- tests/dal_tests/get_all_tests.py
+++ tests/dal_tests/get_all_tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from dal.dal import DAL
-from base.models import Country, AirlineCompany, Flight, Customer, Ticket, Administrator, User
+from base.models import Country, AirlineCompany, Flight, Customer, Ticket, Administrator, User, UserRole
 from datetime import datetime, timedelta
 from django.utils import timezone
 
@@ -16,18 +16,21 @@ class DalGetAllTests(TestCase):
         """ set up class data with instances of classes for the tests methods """
 
         #create user, administrator objects
-        cls.admin_user = User.objects.create(username='ad', password='password', email='ad@gmail.com', user_role='administrator')
-        cls.administrator = Administrator.objects.create(first_name='admin', last_name='instrator', user=cls.admin_user)
+        cls.user_role = UserRole.objects.create(role_name='administrator')
+        cls.admin_user = User.objects.create(username='ad', password='password', email='ad@gmail.com', user_role=cls.user_role)
+        cls.administrator = Administrator.objects.create(first_name='admin', last_name='instrator', user_id=cls.admin_user)
 
 
         # create user, country and airline objects
-        cls.airline_user = User.objects.create(username='Beng', password='password', email='bg@gmail.com', user_role='airline company')
+        cls.airline_role = UserRole.objects.create(role_name="airline company")
+        cls.airline_user = User.objects.create(username='Beng', password='password', email='bg@gmail.com', user_role=cls.airline_role)
         cls.country = Country.objects.create(name='Israel')
-        cls.airline_company = AirlineCompany.objects.create(name='Ben Guryon Airline', country=cls.country, user=cls.airline_user)
+        cls.airline_company = AirlineCompany.objects.create(name='Ben Guryon Airline', country_id=cls.country, user_id=cls.airline_user)
 
         # create user, administrator and customer objects
-        cls.user = User.objects.create(username='avigel', password='password', email='avigel@gmail.com', user_role='customer')
-        cls.customer = Customer.objects.create(first_name='Avigel', last_name='Tzubery', address='Kfar Hanoar, kfar Hsidim bet', phone_number='555-555-5555', credit_card_number='2354624632', user=cls.user)
+        cls.customer_role = UserRole.objects.create(role_name='customer')
+        cls.user = User.objects.create(username='avigel', password='password', email='avigel@gmail.com', user_role=cls.customer_role)
+        cls.customer = Customer.objects.create(first_name='Avigel', last_name='Tzubery', address='Kfar Hanoar, kfar Hsidim bet', phone_no='5555555555', credit_card_no='2354624632', user_id=cls.user)
         
 
         # Create flights objects 
@@ -37,24 +40,26 @@ class DalGetAllTests(TestCase):
         landing_time = departure_time + timedelta(hours=6)
         remaining_tickets = 100
         cls.first_flight = Flight.objects.create(
-            airline_company = DalGetAllTests.airline_company,
-            origin_country = origin_country,
-            destination_country = destination_country,
+            airline_company_id = DalGetAllTests.airline_company,
+            origin_country_id = origin_country,
+            destination_country_id = destination_country,
             departure_time = departure_time,
             landing_time = landing_time,
-            remaining_tickets = remaining_tickets
+            remaining_tickets = remaining_tickets,
+            price = 99
         )
         cls.second_flight = Flight.objects.create(
-            airline_company = DalGetAllTests.airline_company,
-            origin_country = origin_country,
-            destination_country = Country.objects.create(name='Spain'),
+            airline_company_id = DalGetAllTests.airline_company,
+            origin_country_id = origin_country,
+            destination_country_id = Country.objects.create(name='Spain'),
             departure_time = departure_time,
             landing_time = landing_time,
-            remaining_tickets = remaining_tickets
+            remaining_tickets = remaining_tickets,
+            price = 99
         )
 
-        cls.first_ticket = Ticket.objects.create(flight=cls.first_flight, customer=cls.customer)
-        cls.second_ticket = Ticket.objects.create(flight=cls.second_flight, customer=cls.customer)
+        cls.first_ticket = Ticket.objects.create(flight_id=cls.first_flight, customer_id=cls.customer)
+        cls.second_ticket = Ticket.objects.create(flight_id=cls.second_flight, customer_id=cls.customer)
 
 
     
