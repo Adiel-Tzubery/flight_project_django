971d77e3782d6eef7361265916ece89c688db8b9
adiel tzubery <adieltzubery94@gmail.com>
2023-05-30 21:59:21 +0300

    commit: create the facads files and built the facade base structure

---
+++ facads/facade_base.py
@@ -0,0 +1,40 @@
+
+
+
+
+class FacadeBase:
+
+    def get_all_flights():
+        pass
+
+
+    def get_flight_by_id(id):
+        pass
+
+
+    def get_flights_by_parameters(origin_country_id=None, destination_country_id=None, date=None):
+        pass
+
+
+    def get_all_airlines():
+        pass
+
+
+    def get_airline_by_id(id):
+        pass
+
+
+    def get_airline_by_parameters(**enter args):
+        pass
+
+
+    def get_all_countries():
+        pass
+
+
+    def get_country_by_id(id):
+        pass
+
+
+    def create_new_user(**enter args):
+        pass
